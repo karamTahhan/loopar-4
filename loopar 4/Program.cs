26759bb7487f35f54861563a24bb1ca8a4f752c6
karamTahhan <kata07001@utb.vaxjo.se>
2024-12-13 08:59:47 +0100

    Add project files.

---
+++ loopar 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+
+namespace loopar_4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+
+            do
+            {
+                Console.WriteLine("ange lösenord");
+                password = Console.ReadLine();
+
+
+            } while (password != "hemligt");
+            Console.WriteLine("rätt lösenord");
+
+
+        }
+    }
+}
